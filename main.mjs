b1af30557d90c205a74ba81bb9238e141d7b069c
David Veszelovszki <veszelovszki@gmail.com>
2021-10-25 20:00:29 +0200

    Added link to original repo.

--- main.mjs
+++ main.mjs
@@ -1,3 +1,8 @@
+/*
+ * The original version of this came from here: https://github.com/vdavid/facebook-messenger-json-viewer
+ * But this is a completely revamped app.
+ */
+
 import App from './App.mjs';
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
